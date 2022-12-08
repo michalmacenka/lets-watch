4afef22d81863059b3d48281b3eb154b4e1edce1
michalmacenka <my@michalmacenka.cz>
2022-12-08 22:55:22 +0100

    feat: shuffle popular videos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,10 @@
 	import Searchbar from '$lib/components/searchbar/Main.svelte';
 	import { popularVideos } from '$core/store/writable';
 	import PopularVideo from '$lib/components/PopularVideo.svelte';
+	import shuffleArray from '$core/helpers/shuffleArray';
 
 	export let data: PageData;
-	$popularVideos = data.popularVideos;
+	$popularVideos = shuffleArray(data.popularVideos);
 </script>
 
 <main class="flex flex-col items-center p-16 w-full gap-5 ">
@@ -15,7 +16,7 @@
 	</h1>
 	<Searchbar />
 
-	<section class="w-full max-w-6xl ">
+	<section class="w-full max-w-6xl mt-16">
 		<div class="w-full columns   ">
 			{#each $popularVideos as video}
 				<PopularVideo {video} />
