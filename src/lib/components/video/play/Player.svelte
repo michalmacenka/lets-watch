10ffe50c1c8047f1a98c372b751f52a9ef3860a7
michalmacenka <my@michalmacenka.cz>
2023-01-10 21:29:54 +0100

    fix: video switch - subtitles changing

--- src/lib/components/video/play/Player.svelte
+++ src/lib/components/video/play/Player.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-	import Plyr from 'plyr';
-	import 'plyr/dist/plyr.css';
+	//@ts-ignore
+	import { Plyr } from 'svelte-plyr';
 	import { default as toWebVTT } from 'srt-webvtt';
 
 	import { blur } from 'svelte/transition';
-	import { onMount } from 'svelte';
 
 	import { getVideo, getSubtitles } from '$core/services/video';
 	import type * as SV from '$core/schemas/video';
@@ -14,11 +13,13 @@
 	export let videoResolution: number[];
 
 	let player: any;
-	let playerElement: HTMLElement;
+
+	let playerVideos: { src: string; size: number }[] = [];
+	let playerSubtitles: { src: string; label: string }[] = [];
 
 	const initSubtitles = async (src: string) => {
 		return new Promise(async (resolve) => {
-			resolve(URL.createObjectURL(await getSubtitles(src, videoResult.video.site)));
+			resolve(await toWebVTT(await getSubtitles(src, videoResult.video.site)));
 		});
 	};
 
@@ -39,31 +40,46 @@
 			}
 		}
 
-		player.source = {
-			type: 'video',
-			title: $videoInfo.title,
-			sources: videoData.video,
-			tracks: videoData.subtitles
-		};
-		console.log(videoData.subtitles);
+		playerVideos = videoData.video;
+		playerSubtitles = videoData.subtitles;
 		videoResolution = videoData.video.map(({ size }) => size);
 	};
-
-	onMount(() => {
-		player = new Plyr(playerElement, {
-			iconUrl: '/sprite.svg',
-			blankVideo: '',
-			captions: { update: true, active: true }
-		});
-		player.on('captionsenabled', (e: any) => console.log(e.detail.plyr.captions));
-	});
 
 	$: initVideo(videoResult);
 </script>
 
 <div class="my-5" in:blur={{ duration: 500 }}>
 	<!-- svelte-ignore a11y-media-has-caption -->
-	<video bind:this={playerElement} controls playsinline />
+	{#if playerSubtitles[0]}
+		{#key playerVideos}
+			<Plyr
+				bind:player
+				options={{
+					iconUrl: '/sprite.svg',
+					blankVideo: '',
+					captions: { update: true, active: true, language: 'auto' }
+				}}
+			>
+				<video controls playsinline>
+					{#each playerVideos as { src, size }}
+						<source {src} type="video/mp4" {size} />
+					{/each}
+
+					{#each playerSubtitles as { src, label }}
+						<track kind="captions" {label} {src} />
+					{/each}
+				</video>
+			</Plyr>
+		{/key}
+	{:else}
+		<div
+			class="flex items-center justify-center w-full gap-2 font-semibold text-light text-xl my-20"
+			in:blur
+		>
+			<i class="ri-loader-4-line animate-spin" />
+			<h2>Loading Video</h2>
+		</div>
+	{/if}
 </div>
 
 <style lang="scss">
